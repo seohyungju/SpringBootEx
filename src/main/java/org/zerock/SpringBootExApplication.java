533721d8b98f2bb587daa93f8954ddf0dbc64882
SEO <shj890917@naver.com>
2018-10-12 23:15:30 +0900

    Filter, Interceptor 예제 적용 및  Thymeleaf 예제 작성

--- src/main/java/org/zerock/SpringBootExApplication.java
+++ src/main/java/org/zerock/SpringBootExApplication.java
@@ -1,8 +1,13 @@
 package org.zerock;
 
+import javax.servlet.Filter;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.zerock.filter.SampleFilter;
 
 @SpringBootApplication
 @MapperScan(value = {"org.zerock.mapper"})
@@ -10,5 +15,17 @@ public class SpringBootExApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean someFilterRegistration(){
+		
+		FilterRegistrationBean registration = new FilterRegistrationBean<>();
+		SampleFilter sampleFilter = new SampleFilter();
+		registration.setFilter(sampleFilter);
+		registration.addUrlPatterns("/test/*");
+		registration.setName("sampleFilter");
+		
+		return registration;
 	}
 }
